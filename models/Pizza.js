099c0bb3a8e10575a97bbf272e29032a8b8aedb4
Neo <fullstackneo@gmail.com>
2022-05-22 14:40:05 -0600

    add comment model and virtual to pizza model

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,20 +1,41 @@
 const { Schema, model } = require('mongoose');
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String,
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    size: {
+      type: String,
+      default: 'Large',
+    },
+    toppings: [],
+    comments: [
+      {
+        // type: String,
+        type: Schema.Types.ObjectId,
+        ref: 'Comment',
+      },
+    ],
   },
-  createdBy: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  size: {
-    type: String,
-    default: 'Large',
-  },
-  toppings: [],
+  //  tell the schema that it can use virtuals
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function () {
+  return this.comments.length;
 });
 
 // create the Pizza model using the PizzaSchema
